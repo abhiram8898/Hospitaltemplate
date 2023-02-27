a11c9422035c714780d39016162575a2bd82961b
Abhiram <abhiravi889833@gmail.com>
2023-02-27 14:58:20 +0530

    commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function validate() {
+    email = document.getElementById("email").value;
+    var e =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email == "") {
+        alert("enter mail");
+        return false;
+    } else if (!e.test(email)) {
+        alert("enter valid mail addres");
+        return false;
+    }
+    return true;
+}
